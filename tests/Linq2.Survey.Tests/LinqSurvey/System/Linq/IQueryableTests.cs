cc1a96b23cd26bd3dc96048bf00f132fb7d8bbc3
rstam <robert@robertstam.org>
2021-02-22 11:12:16 -0800

    Aggregate is not supported.

--- tests/Linq2.Survey.Tests/LinqSurvey/System/Linq/IQueryableTests.cs
+++ tests/Linq2.Survey.Tests/LinqSurvey/System/Linq/IQueryableTests.cs
@@ -13,6 +13,42 @@ namespace Linq2.Survey.Tests.LinqSurvey.System.Linq
     {
         // public methods
         [Fact]
+        public void Aggregate_is_not_supported()
+        {
+            var documents = new[] { "{ _id : 1, X : 1 }", "{ _id : 2, X : 2 }" };
+            var collection = CreateCollection<DocumentWithInt>(documents: documents);
+            var subject = collection.AsQueryable();
+
+            var exception = Record.Exception(() => subject.Aggregate((a, d) => new DocumentWithInt { Id = 0, X = a.X + d.X }));
+
+            exception.Should().BeOfType<NotSupportedException>();
+        }
+
+        [Fact]
+        public void Aggregate_with_seed_is_not_supported()
+        {
+            var documents = new[] { "{ _id : 1, X : 1 }", "{ _id : 2, X : 2 }" };
+            var collection = CreateCollection<DocumentWithInt>(documents: documents);
+            var subject = collection.AsQueryable();
+
+            var exception = Record.Exception(() => subject.Aggregate(0, (a, d) => a + d.X));
+
+            exception.Should().BeOfType<NotSupportedException>();
+        }
+
+        [Fact]
+        public void Aggregate_with_seed_and_selector_is_not_supported()
+        {
+            var documents = new[] { "{ _id : 1, X : 1 }", "{ _id : 2, X : 2 }" };
+            var collection = CreateCollection<DocumentWithInt>(documents: documents);
+            var subject = collection.AsQueryable();
+
+            var exception = Record.Exception(() => subject.Aggregate(new DocumentWithInt { X = 0 }, (a, d) => new DocumentWithInt { Id = 0, X = a.X + d.X }, a => a.X));
+
+            exception.Should().BeOfType<NotSupportedException>();
+        }
+
+        [Fact]
         public void All_is_not_supported()
         {
             var documents = new[] { "{ _id : 1, X : 1 }", "{ _id : 2, X : 2 }" };
