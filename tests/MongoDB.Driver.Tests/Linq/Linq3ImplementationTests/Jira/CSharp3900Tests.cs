05f89ccaccdd30ba29fc33f2f9a5b728c0a95d2a
rstam <robert@robertstam.org>
2021-10-14 13:49:17 -0700

    CSHARP-3900: Reproduction.

---
+++ tests/MongoDB.Driver.Tests/Linq/Linq3ImplementationTests/Jira/CSharp3900Tests.cs
@@ -0,0 +1,224 @@
+﻿/* Copyright 2010-present MongoDB Inc.
+*
+* Licensed under the Apache License, Version 2.0 (the "License");
+* you may not use this file except in compliance with the License.
+* You may obtain a copy of the License at
+*
+* http://www.apache.org/licenses/LICENSE-2.0
+*
+* Unless required by applicable law or agreed to in writing, software
+* distributed under the License is distributed on an "AS IS" BASIS,
+* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+* See the License for the specific language governing permissions and
+* limitations under the License.
+*/
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Xunit;
+
+namespace MongoDB.Driver.Tests.Linq.Linq3ImplementationTests.Jira
+{
+    public class CSharp3900Tests
+    {
+        [Fact]
+        public void ByteArray_comparison_should_work()
+        {
+            var collection = GetCollection();
+
+            var queryable = collection.AsQueryable()
+                .Where(x => x.ByteArray[0] == 1);
+
+            var stages = Linq3TestHelpers.Translate(collection, queryable);
+            var expectedStages = new[]
+            {
+                "{ $match : { 'ByteArray.0' : 1 } }"
+            };
+            Linq3TestHelpers.AssertStages(stages, expectedStages);
+        }
+
+        [Fact]
+        public void DecimalArray_comparison_should_work()
+        {
+            var collection = GetCollection();
+
+            var queryable = collection.AsQueryable()
+                .Where(x => x.DecimalArray[0] == 1);
+
+            var stages = Linq3TestHelpers.Translate(collection, queryable);
+            var expectedStages = new[]
+            {
+                "{ $match : { 'DecimalArray.0' : '1' } }"
+            };
+            Linq3TestHelpers.AssertStages(stages, expectedStages);
+        }
+
+        [Fact]
+        public void DoubleArray_comparison_should_work()
+        {
+            var collection = GetCollection();
+
+            var queryable = collection.AsQueryable()
+                .Where(x => x.DoubleArray[0] == 1);
+
+            var stages = Linq3TestHelpers.Translate(collection, queryable);
+            var expectedStages = new[]
+            {
+                "{ $match : { 'DoubleArray.0' : 1.0 } }"
+            };
+            Linq3TestHelpers.AssertStages(stages, expectedStages);
+        }
+
+        [Fact]
+        public void Int16Array_comparison_should_work()
+        {
+            var collection = GetCollection();
+
+            var queryable = collection.AsQueryable()
+                .Where(x => x.Int16Array[0] == 1);
+
+            var stages = Linq3TestHelpers.Translate(collection, queryable);
+            var expectedStages = new[]
+            {
+                "{ $match : { 'Int16Array.0' : 1 } }"
+            };
+            Linq3TestHelpers.AssertStages(stages, expectedStages);
+        }
+
+        [Fact]
+        public void Int32Array_comparison_should_work()
+        {
+            var collection = GetCollection();
+
+            var queryable = collection.AsQueryable()
+                .Where(x => x.Int32Array[0] == 1);
+
+            var stages = Linq3TestHelpers.Translate(collection, queryable);
+            var expectedStages = new[]
+            {
+                "{ $match : { 'Int32Array.0' : 1 } }"
+            };
+            Linq3TestHelpers.AssertStages(stages, expectedStages);
+        }
+
+        [Fact]
+        public void Int64Array_comparison_should_work()
+        {
+            var collection = GetCollection();
+
+            var queryable = collection.AsQueryable()
+                .Where(x => x.Int64Array[0] == 1);
+
+            var stages = Linq3TestHelpers.Translate(collection, queryable);
+            var expectedStages = new[]
+            {
+                "{ $match : { 'Int64Array.0' : 1 } }"
+            };
+            Linq3TestHelpers.AssertStages(stages, expectedStages);
+        }
+
+        [Fact]
+        public void SByteArray_comparison_should_work()
+        {
+            var collection = GetCollection();
+
+            var queryable = collection.AsQueryable()
+                .Where(x => x.SByteArray[0] == 1);
+
+            var stages = Linq3TestHelpers.Translate(collection, queryable);
+            var expectedStages = new[]
+            {
+                "{ $match : { 'SByteArray.0' : 1.0 } }"
+            };
+            Linq3TestHelpers.AssertStages(stages, expectedStages);
+        }
+
+        [Fact]
+        public void SingleArray_comparison_should_work()
+        {
+            var collection = GetCollection();
+
+            var queryable = collection.AsQueryable()
+                .Where(x => x.SingleArray[0] == 1);
+
+            var stages = Linq3TestHelpers.Translate(collection, queryable);
+            var expectedStages = new[]
+            {
+                "{ $match : { 'SingleArray.0' : 1.0 } }"
+            };
+            Linq3TestHelpers.AssertStages(stages, expectedStages);
+        }
+
+        [Fact]
+        public void UInt16Array_comparison_should_work()
+        {
+            var collection = GetCollection();
+
+            var queryable = collection.AsQueryable()
+                .Where(x => x.UInt16Array[0] == 1);
+
+            var stages = Linq3TestHelpers.Translate(collection, queryable);
+            var expectedStages = new[]
+            {
+                "{ $match : { 'UInt16Array.0' : 1 } }"
+            };
+            Linq3TestHelpers.AssertStages(stages, expectedStages);
+        }
+
+        [Fact]
+        public void UInt32Array_comparison_should_work()
+        {
+            var collection = GetCollection();
+
+            var queryable = collection.AsQueryable()
+                .Where(x => x.UInt32Array[0] == 1);
+
+            var stages = Linq3TestHelpers.Translate(collection, queryable);
+            var expectedStages = new[]
+            {
+                "{ $match : { 'UInt32Array.0' : 1 } }"
+            };
+            Linq3TestHelpers.AssertStages(stages, expectedStages);
+        }
+
+        [Fact]
+        public void UInt64Array_comparison_should_work()
+        {
+            var collection = GetCollection();
+
+            var queryable = collection.AsQueryable()
+                .Where(x => x.UInt64Array[0] == 1);
+
+            var stages = Linq3TestHelpers.Translate(collection, queryable);
+            var expectedStages = new[]
+            {
+                "{ $match : { 'UInt64Array.0' : 1 } }"
+            };
+            Linq3TestHelpers.AssertStages(stages, expectedStages);
+        }
+
+        private IMongoCollection<C> GetCollection()
+        {
+            var client = DriverTestConfiguration.Linq3Client;
+            var database = client.GetDatabase(DriverTestConfiguration.DatabaseNamespace.DatabaseName);
+            return database.GetCollection<C>(DriverTestConfiguration.CollectionNamespace.CollectionName);
+        }
+
+        private class C
+        {
+            public byte[] ByteArray { get; set; }
+            public decimal[] DecimalArray { get; set; }
+            public double[] DoubleArray { get; set; }
+            public short[] Int16Array { get; set; }
+            public int[] Int32Array { get; set; }
+            public long[] Int64Array { get; set; }
+            public sbyte[] SByteArray { get; set; }
+            public float[] SingleArray { get; set; }
+            public ushort[] UInt16Array { get; set; }
+            public uint[] UInt32Array { get; set; }
+            public ulong[] UInt64Array { get; set; }
+        }
+    }
+}
