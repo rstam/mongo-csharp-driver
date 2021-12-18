cf626f268834f261c86fe4301d5f95093bbf8575
rstam <robert@robertstam.org>
2021-12-17 16:13:15 -0800

    CSHARP-3989: Setup OData host and attempt to repro.

---
+++ tests/ODataHost/Startup.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.AspNet.OData.Builder;
+using Microsoft.AspNet.OData.Extensions;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace ODataHost
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddMvc(opt => opt.EnableEndpointRouting = false);
+            services.AddOData();
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            var builder = new ODataConventionModelBuilder(app.ApplicationServices);
+            builder.EntitySet<Product>("Products");
+
+            app.UseMvc(routeBuilder =>
+            {
+                routeBuilder.Select().Expand().Filter().OrderBy().MaxTop(100).Count();
+                routeBuilder.MapODataServiceRoute("ODataRoute", "odata", builder.GetEdmModel());
+            });
+        }
+    }
+}
