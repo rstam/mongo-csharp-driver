6e43bbaa939bd146feadc3753259ea8a3968b507
rstam <robert@robertstam.org>
2021-12-17 16:13:15 -0800

    CSHARP-3989: Setup OData host and attempt to repro.

---
+++ tests/ODataHost/Controllers/ProductsController.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.AspNet.OData;
+using MongoDB.Driver;
+using MongoDB.Driver.Linq;
+
+namespace ODataHost.Controllers
+{
+    public class ProductsController : ODataController
+    {
+        private MongoClient _client;
+        private IMongoDatabase _db;
+        private IMongoCollection<Product> _coll;
+
+        public ProductsController()
+        {
+            var clientSettings = MongoClientSettings.FromConnectionString("mongodb://localhost");
+            clientSettings.LinqProvider = LinqProvider.V3;
+            _client = new MongoClient(clientSettings);
+            _db = _client.GetDatabase("test");
+            _coll = _db.GetCollection<Product>("products");
+        }
+
+        [EnableQuery]
+        public IQueryable<Product> Get()
+        {
+            return _coll.AsQueryable();
+        }
+    }
+}
---
+++ tests/ODataHost/Product.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MongoDB.Bson.Serialization.Attributes;
+
+namespace ODataHost
+{
+    public class Product
+    {
+        public int Id { get; set; }
+        [BsonElement("name")]
+        public string Name { get; set; }
+    }
+}
