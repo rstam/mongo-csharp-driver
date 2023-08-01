bc3ce69e4bfd4b7ac233444a8eeab812848634b3
rstam <robert@robertstam.org>
2023-08-01 16:29:49 -0700

    Identity projection prototype.

---
+++ tests/MongoDB.Driver.Tests/Linq/Linq3Implementation/Jira/IdentityProjectionTests.cs
@@ -0,0 +1,80 @@
+﻿/* Copyright 2010-present MongoDB Inc.
+*
+* Licensed under the Apache License, Version 2.0 (the "License");
+* you may not use this file except in compliance with the License.
+* You may obtain a copy of the License at
+*
+* http://www.apache.org/licenses/LICENSE-2.0
+*
+* Unless required by applicable law or agreed to in writing, software
+* distributed under the License is distributed on an "AS IS" BASIS,
+* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+* See the License for the specific language governing permissions and
+* limitations under the License.
+*/
+
+using FluentAssertions;
+using MongoDB.Driver.Linq;
+using MongoDB.TestHelpers.XunitExtensions;
+using Xunit;
+
+namespace MongoDB.Driver.Tests.Linq.Linq3Implementation.Jira
+{
+    public class IdentityProjectionTests : Linq3IntegrationTest
+    {
+        [Theory]
+        [ParameterAttributeData]
+        public void Find_with_identity_projection_should_work(
+            [Values(LinqProvider.V2, LinqProvider.V3)] LinqProvider linqProvider)
+        {
+            var collection = GetCollection(linqProvider);
+            var projection = Builders<C>.Projection.Expression(x => x);
+
+            var find = collection
+                .Find("{}")
+                .Project(projection);
+
+            var renderedProjection = TranslateFindProjection(collection, find);
+            renderedProjection.Should().BeNull();
+
+            var result = find.Single();
+            result.Id.Should().Be(1);
+            result.X.Should().Be(2);
+        }
+
+        [Theory]
+        [ParameterAttributeData]
+        public void Aggregate_Project_with_identity_projection_should_work(
+            [Values(LinqProvider.V2, LinqProvider.V3)] LinqProvider linqProvider)
+        {
+            var collection = GetCollection(linqProvider);
+            var projection = Builders<C>.Projection.Expression(x => x);
+
+            var aggregate = collection
+                .Aggregate()
+                .Project(projection);
+
+            var stages = Translate(collection, aggregate);
+            stages.Should().BeEmpty();
+
+            var result = aggregate.Single();
+            result.Id.Should().Be(1);
+            result.X.Should().Be(2);
+        }
+
+        private IMongoCollection<C> GetCollection(LinqProvider linqProvider)
+        {
+            var collection = GetCollection<C>("test", linqProvider);
+            CreateCollection(
+                collection,
+                new C { Id = 1, X = 2 });
+            return collection;
+        }
+
+        private class C
+        {
+            public int Id { get; set; }
+            public int X { get; set; }
+        }
+    }
+}
