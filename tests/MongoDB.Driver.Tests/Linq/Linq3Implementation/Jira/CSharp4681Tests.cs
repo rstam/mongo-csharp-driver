e97d86b181c5c2c45f618010ffb0bd1cc941b7b2
rstam <robert@robertstam.org>
2023-08-02 17:20:54 -0700

    CSHARP-4681: All the test cases I could think of using projections.

--- tests/MongoDB.Driver.Tests/Linq/Linq3Implementation/Jira/CSharp4681Tests.cs
+++ tests/MongoDB.Driver.Tests/Linq/Linq3Implementation/Jira/CSharp4681Tests.cs
@@ -13,6 +13,7 @@
 * limitations under the License.
 */
 
+using System;
 using FluentAssertions;
 using MongoDB.Bson.Serialization;
 using MongoDB.Driver.Linq;
@@ -25,7 +26,224 @@ namespace MongoDB.Driver.Tests.Linq.Linq3Implementation.Jira
     {
         [Theory]
         [ParameterAttributeData]
-        public void ExpressionProjectionDefinition_with_field_projection_Render_should_work(
+        public void Find_Project_should_work(
+            [Values(LinqProvider.V2, LinqProvider.V3)] LinqProvider linqProvider)
+        {
+            var collection = GetCollection(linqProvider);
+
+            var find = collection
+                .Find("{}")
+                .Project(x => x.Id);
+
+            var projection = TranslateFindProjection(collection, find);
+            projection.Should().Be("{ _id : 1 }");
+
+            var result = find.Single();
+            result.Should().Be(1);
+        }
+
+        [Theory]
+        [ParameterAttributeData]
+        public void FindSync_with_Builder_Expression_should_work(
+            [Values(LinqProvider.V2, LinqProvider.V3)] LinqProvider linqProvider)
+        {
+            var collection = GetCollection(linqProvider);
+            var projection = Builders<C>.Projection.Expression(x => x.Id);
+            var options = new FindOptions<C, int> { Projection = projection };
+
+            var result = collection.FindSync("{}", options).Single();
+
+            result.Should().Be(1);
+        }
+
+        [Theory]
+        [ParameterAttributeData]
+        public void FindSync_with_new_ExpressionProjectionDefinition_should_work(
+            [Values(LinqProvider.V2, LinqProvider.V3)] LinqProvider linqProvider)
+        {
+            var collection = GetCollection(linqProvider);
+            var projection = new ExpressionProjectionDefinition<C, int>(x => x.Id, translationOptions: null); // note: ExpressionProjectionDefinition is internal
+            var options = new FindOptions<C, int> { Projection = projection };
+
+            var result = collection.FindSync("{}", options).Single();
+
+            result.Should().Be(1);
+        }
+
+        [Theory]
+        [ParameterAttributeData]
+        public void FindSync_with_new_FindExpressionProjectionDefinition_should_work(
+            [Values(LinqProvider.V2, LinqProvider.V3)] LinqProvider linqProvider)
+        {
+            var collection = GetCollection(linqProvider);
+            var projection = new FindExpressionProjectionDefinition<C, int>(x => x.Id);
+            var options = new FindOptions<C, int> { Projection = projection };
+
+            var result = collection.FindSync("{}", options).Single();
+
+            result.Should().Be(1);
+        }
+
+        [Theory]
+        [ParameterAttributeData]
+        public void FindOneAndDelete_with_Builder_Expression_should_work(
+            [Values(LinqProvider.V2, LinqProvider.V3)] LinqProvider linqProvider)
+        {
+            var collection = GetCollection(linqProvider);
+            var projection = Builders<C>.Projection.Expression(x => x.Id);
+            var options = new FindOneAndDeleteOptions<C, int> { Projection = projection };
+
+            var result = collection.FindOneAndDelete(x => x.Id == 1, options);
+
+            result.Should().Be(1);
+        }
+
+        [Theory]
+        [ParameterAttributeData]
+        public void FindOneAndDelete_with_new_ExpressionProjectionDefinition_should_work(
+            [Values(LinqProvider.V2, LinqProvider.V3)] LinqProvider linqProvider)
+        {
+            var collection = GetCollection(linqProvider);
+            var projection = new ExpressionProjectionDefinition<C, int>(x => x.Id, translationOptions: null); // note: ExpressionProjectionDefinition is internal
+            var options = new FindOneAndDeleteOptions<C, int> { Projection = projection };
+
+            var result = collection.FindOneAndDelete(x => x.Id == 1, options);
+
+            result.Should().Be(1);
+        }
+
+        [Theory]
+        [ParameterAttributeData]
+        public void FindOneAndDelete_with_new_FindExpressionProjectionDefinition_should_work(
+            [Values(LinqProvider.V2, LinqProvider.V3)] LinqProvider linqProvider)
+        {
+            var collection = GetCollection(linqProvider);
+            var projection = new FindExpressionProjectionDefinition<C, int>(x => x.Id);
+            var options = new FindOneAndDeleteOptions<C, int> { Projection = projection };
+
+            var result = collection.FindOneAndDelete(x => x.Id == 1, options);
+
+            result.Should().Be(1);
+        }
+
+        [Theory]
+        [ParameterAttributeData]
+        public void FindOneAndReplace_with_Builder_Expression_should_work(
+            [Values(LinqProvider.V2, LinqProvider.V3)] LinqProvider linqProvider)
+        {
+            var collection = GetCollection(linqProvider);
+            var replacement = new C { Id = 1 };
+            var projection = Builders<C>.Projection.Expression(x => x.Id);
+            var options = new FindOneAndReplaceOptions<C, int> { Projection = projection };
+
+            var result = collection.FindOneAndReplace(x => x.Id == 1, replacement, options);
+
+            result.Should().Be(1);
+        }
+
+        [Theory]
+        [ParameterAttributeData]
+        public void FindOneAndReplace_with_new_ExpressionProjectionDefinition_should_work(
+            [Values(LinqProvider.V2, LinqProvider.V3)] LinqProvider linqProvider)
+        {
+            var collection = GetCollection(linqProvider);
+            var replacement = new C { Id = 1 };
+            var projection = new ExpressionProjectionDefinition<C, int>(x => x.Id, translationOptions: null); // note: ExpressionProjectionDefinition is internal
+            var options = new FindOneAndReplaceOptions<C, int> { Projection = projection };
+
+            var result = collection.FindOneAndReplace(x => x.Id == 1, replacement, options);
+
+            result.Should().Be(1);
+        }
+
+        [Theory]
+        [ParameterAttributeData]
+        public void FindOneAndReplace_with_new_FindExpressionProjectionDefinition_should_work(
+            [Values(LinqProvider.V2, LinqProvider.V3)] LinqProvider linqProvider)
+        {
+            var collection = GetCollection(linqProvider);
+            var replacement = new C { Id = 1 };
+            var projection = new FindExpressionProjectionDefinition<C, int>(x => x.Id);
+            var options = new FindOneAndReplaceOptions<C, int> { Projection = projection };
+
+            var result = collection.FindOneAndReplace(x => x.Id == 1, replacement, options);
+
+            result.Should().Be(1);
+        }
+
+        [Theory]
+        [ParameterAttributeData]
+        public void FindOneAndUpdate_with_Builder_Expression_should_work(
+            [Values(LinqProvider.V2, LinqProvider.V3)] LinqProvider linqProvider)
+        {
+            var collection = GetCollection(linqProvider);
+            var update = Builders<C>.Update.Set(x => x.Id, 1);
+            var projection = Builders<C>.Projection.Expression(x => x.Id);
+            var options = new FindOneAndUpdateOptions<C, int> { Projection = projection };
+
+            var result = collection.FindOneAndUpdate(x => x.Id == 1, update, options);
+
+            result.Should().Be(1);
+        }
+
+        [Theory]
+        [ParameterAttributeData]
+        public void FindOneAndUpdate_with_new_ExpressionProjectionDefinition_should_work(
+            [Values(LinqProvider.V2, LinqProvider.V3)] LinqProvider linqProvider)
+        {
+            var collection = GetCollection(linqProvider);
+            var update = Builders<C>.Update.Set(x => x.Id, 1);
+            var projection = new ExpressionProjectionDefinition<C, int>(x => x.Id, translationOptions: null); // note: ExpressionProjectionDefinition is internal
+            var options = new FindOneAndUpdateOptions<C, int> { Projection = projection };
+
+            var result = collection.FindOneAndUpdate(x => x.Id == 1, update, options);
+
+            result.Should().Be(1);
+        }
+
+        [Theory]
+        [ParameterAttributeData]
+        public void FindOneAndUpdate_with_new_FindExpressionProjectionDefinition_should_work(
+            [Values(LinqProvider.V2, LinqProvider.V3)] LinqProvider linqProvider)
+        {
+            var collection = GetCollection(linqProvider);
+            var update = Builders<C>.Update.Set(x => x.Id, 1);
+            var projection = new FindExpressionProjectionDefinition<C, int>(x => x.Id);
+            var options = new FindOneAndUpdateOptions<C, int> { Projection = projection };
+
+            var result = collection.FindOneAndUpdate(x => x.Id == 1, update, options);
+
+            result.Should().Be(1);
+        }
+
+        [Theory]
+        [ParameterAttributeData]
+        public void Aggregate_Project_should_work(
+            [Values(LinqProvider.V2, LinqProvider.V3)] LinqProvider linqProvider)
+        {
+            var collection = GetCollection(linqProvider);
+
+            var aggregate = collection
+                .Aggregate()
+                .Project(x => x.Id);
+
+            var stages = Translate(collection, aggregate);
+            if (linqProvider == LinqProvider.V2)
+            {
+                AssertStages(stages, "{ $project : { _id : 1 } }");
+            }
+            else
+            {
+                AssertStages(stages, "{ $project : { _v : '$_id', _id : 0 } }");
+            }
+
+            var result = aggregate.Single();
+            result.Should().Be(1);
+        }
+
+        [Theory]
+        [ParameterAttributeData]
+        public void ExpressionProjectionDefinition_Render_should_work(
             [Values(LinqProvider.V2, LinqProvider.V3)] LinqProvider linqProvider)
         {
             var projectionDefinition = new ExpressionProjectionDefinition<C, int>(x => x.Id, translationOptions: null);
@@ -39,7 +257,7 @@ namespace MongoDB.Driver.Tests.Linq.Linq3Implementation.Jira
 
         [Theory]
         [ParameterAttributeData]
-        public void ExpressionProjectionDefinition_with_field_projection_RenderForFind_should_work(
+        public void ExpressionProjectionDefinition_RenderForFind_should_work(
             [Values(LinqProvider.V2, LinqProvider.V3)] LinqProvider linqProvider)
         {
             var projectionDefinition = new ExpressionProjectionDefinition<C, int>(x => x.Id, translationOptions: null);
@@ -53,7 +271,7 @@ namespace MongoDB.Driver.Tests.Linq.Linq3Implementation.Jira
 
         [Theory]
         [ParameterAttributeData]
-        public void FindExpressionProjectionDefinition_with_field_projection_Render_should_work(
+        public void FindExpressionProjectionDefinition_Render_should_work(
             [Values(LinqProvider.V2, LinqProvider.V3)] LinqProvider linqProvider)
         {
             var projectionDefinition = new FindExpressionProjectionDefinition<C, int>(x => x.Id);
@@ -67,7 +285,7 @@ namespace MongoDB.Driver.Tests.Linq.Linq3Implementation.Jira
 
         [Theory]
         [ParameterAttributeData]
-        public void FindExpressionProjectionDefinition_with_field_projection_RenderForFind_should_work(
+        public void FindExpressionProjectionDefinition_RenderForFind_should_work(
             [Values(LinqProvider.V2, LinqProvider.V3)] LinqProvider linqProvider)
         {
             var projectionDefinition = new FindExpressionProjectionDefinition<C, int>(x => x.Id);
@@ -77,6 +295,15 @@ namespace MongoDB.Driver.Tests.Linq.Linq3Implementation.Jira
             var renderedProjection = projectionDefinition.RenderForFind(sourceSerializer, serializerRegistry, linqProvider);
 
             renderedProjection.Document.Should().Be("{ _id : 1 }");
+        }
+
+        private IMongoCollection<C> GetCollection(LinqProvider linqProvider)
+        {
+            var collection = GetCollection<C>("test", linqProvider);
+            CreateCollection(
+                collection,
+                new C { Id = 1 });
+            return collection;
         }
 
         private class C
