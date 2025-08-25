2403d259bebe6c1baed5f2d2b03880eb8f9f0b5c
rstam <robert@mongodb.com>
2025-08-25 16:17:44 -0400

    test

--- tests/MongoDB.Driver.Tests/AggregateBucketAutoResultEqualityComparer.cs
+++ tests/MongoDB.Driver.Tests/AggregateBucketAutoResultEqualityComparer.cs
@@ -1,4 +1,6 @@
-﻿/* Copyright 2016-present MongoDB Inc.
+﻿// Test signing key
+
+/* Copyright 2016-present MongoDB Inc.
 *
 * Licensed under the Apache License, Version 2.0 (the "License");
 * you may not use this file except in compliance with the License.
